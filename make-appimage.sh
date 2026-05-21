63bc82bb1f947c84f7a9754c257122f864d02732
Link4Electronics <link4electronics@gmail.com>
2026-05-21 01:28:19 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,22 +3,31 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q PACKAGENAME | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q flacon | awk '{print $2; exit}')
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=PATH_OR_URL_TO_ICON
-export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export ICON=/usr/share/icons/hicolor/scalable/apps/flacon.svg
+export DESKTOP=/usr/share/applications/flacon.desktop
+export STARTUPWMCLASS=flacon
 
 # Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
-
-# Additional changes can be done in between here
+quick-sharun /usr/bin/flacon \
+  /usr/bin/alacenc \
+  /usr/bin/faac \
+  /usr/bin/flac \
+  /usr/bin/lame \
+  /usr/bin/mac \
+  /usr/bin/oggenc \
+  /usr/bin/opusenc \
+  /usr/bin/sox \
+  /usr/bin/wavpack \
+  /usr/bin/wvunpack
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
 
 # Test the app for 12 seconds, if the test fails due to the app
 # having issues running in the CI use --simple-test instead
-quick-sharun --test ./dist/*.AppImage
+quick-sharun --simple-test ./dist/*.AppImage
