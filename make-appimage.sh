3932e1a26ec591a3fe6b272f866bea92a28fc7e1
Link4Electronics <link4electronics@gmail.com>
2026-08-30 06:32:06 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -11,8 +11,6 @@ export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}
 export ICON=/usr/share/icons/hicolor/scalable/apps/flacon.svg
 export DESKTOP=/usr/share/applications/flacon.desktop
 export STARTUPWMCLASS=flacon
-export DEPLOY_QT=1
-export QT_DIR=qt6
 
 # Deploy dependencies
 quick-sharun /usr/bin/flacon \
@@ -32,4 +30,4 @@ quick-sharun --make-appimage
 
 # Test the app for 12 seconds, if the test fails due to the app
 # having issues running in the CI use --simple-test instead
-quick-sharun --test ./dist/*.AppImage
+quick-sharun --simple-test ./dist/*.AppImage
